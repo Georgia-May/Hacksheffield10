af231ccbd65bb700dbb1826524529c0b6a6151aa
Georgia-May <georgiamaybetts@gmail.com>
2025-11-29 15:15:39 +0000

    yah

--- src/main/java/com/oddessey/oddesseymod/Config.java
+++ src/main/java/com/oddessey/oddesseymod/Config.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.oddessey.oddesseymod;
 
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
--- src/main/java/com/oddessey/oddesseymod/OddesseyMod.java
+++ src/main/java/com/oddessey/oddesseymod/OddesseyMod.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.oddessey.oddesseymod;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
